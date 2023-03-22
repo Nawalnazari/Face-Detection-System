54c18e5eb2e7cc0ac9fedc79efeed97fefb8f66a
Nawalnazari <86686447+Nawalnazari@users.noreply.github.com>
2023-03-22 16:30:59 +0800

    Add files via upload

---
+++ Face_detection.py
@@ -0,0 +1,37 @@
+import cv2
+
+# Load the cascade classifier
+#Insert your file path
+face_cascade = cv2.CascadeClassifier("C:/Users/SamChan/Documents/FYP/FYP/FYP Project/Cascades/haarcascade_frontalface_default.xml")
+
+# Initialize the webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    # Read the frame from the webcam
+    ret, frame = cap.read()
+
+    # Convert the frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+
+    # Draw rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    # Display the output
+    cv2.imshow('Live Face Detection', frame)
+
+    # Exit the loop if the 'q' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# Release the webcam
+cap.release()
+
+# Close all windows
+cv2.destroyAllWindows()
+
+
